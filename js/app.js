9b4c3ce9352fcefd9c82174ca5b4ca8c3fd9ec44
Worathiti Manosroi <w.manosroi@gmail.com>
2015-09-26 13:23:14 +0200

    get current location

--- js/app.js
+++ js/app.js
@@ -34,4 +34,42 @@ var myApp = angular.module('myApp', ['ngMap']);
 myApp.controller('AppController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
     console.log($scope.message);
+    
+    
+    //@pung get current location code 
+     $scope.getLocation = function(callback){
+        var stillWaitForUserLocation = true;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+                console.log("user accept location access");
+                stillWaitForUserLocation = false;
+                callback.success(position);
+            },function(){
+                callback.fail();
+            });
+        } else {
+           callback.fail();
+           console.log("Geolocation is not supported by this browser.");
+        }
+        setTimeout(function(){
+            //make sure that after success, do not call fail again..
+            if(stillWaitForUserLocation == true){
+                console.log("set callback fail.");
+                callback.fail();
+            }
+        }, 6000);
+        //TODO: timeout call fail
+    }
+     
+    // todo: time interval getting user location
+    $scope.getLocation({
+            success:function(userPosition){
+              console.log(userPosition);
+              $scope.userLocation = userPosition;
+            },
+            fail:function(){
+                //user denied the location
+            }
+    });
+    
 });
